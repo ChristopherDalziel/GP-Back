10ad94581cb71f8241b3181467a28694b3a74e30
Nhan Dang <nhantdang@gmail.com>
2020-01-17 09:56:26 +1100

    connected to mongodb atlas

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+//heroku won't always use port 5000
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const dbConfig = { useNewUrlParser: true, useUnifiedTopology: true }
+
+//mongoose
+mongoose.connect(process.env.DB_URL, dbConfig, (err) => {
+  if (err) {
+    console.log(`error: ${err.message}`)
+  } else {
+    console.log('connected to MongoDB Atlas')
+  }
+})
+
+//middleware
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
