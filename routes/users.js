2140361ab557f10cf0479866a9d8b7a6f2174a89
Nhan Dang <nhantdang@gmail.com>
2020-01-22 12:06:45 +1100

    users and token routes and controllers updated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {register, login} = require('../controllers/users');
+const {checkToken, checkPasswordToken} = require('../controllers/token_middleware');
 
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/check-token', checkToken);
+router.get('/check-password-token', checkPasswordToken);
+router.put('/reset-password', express.json(), resetPassword)
+
 
 module.exports = router;
