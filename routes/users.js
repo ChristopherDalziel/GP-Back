04ddf1d868077823258141b493c437e5f0ee6896
Nhan Dang <nhantdang@gmail.com>
2020-01-23 15:50:05 +1100

    password token added to user model

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {register, login, resetPassword, findUser} = require('../controllers/users');
 const {checkToken, checkPasswordToken} = require('../controllers/token_middleware');
-const {checkEmail} = require('../controllers/users_middleware');
+
 
 
 
@@ -11,7 +11,7 @@ router.post('/login', login);
 router.get('/check-token', checkToken);
 router.get('/check-password-token', checkPasswordToken);
 router.get('/find-user/:token', findUser)
-router.put('/reset-password', express.json(), checkEmail, resetPassword)
+
 
 
 module.exports = router;
