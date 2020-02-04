5d65f8e467d0f5ff82ba460dc88d5f8c4f8a87b7
Cai heng <ianyaheng822@gmail.com>
2020-02-04 16:04:42 +1100

    add the vaccines test

--- routes/openingHours.test.js
+++ routes/openingHours.test.js
@@ -2,10 +2,11 @@ const app = require("../app");
 const supertest = require('supertest');
 const request = supertest(app);
 const mongoose = require("mongoose");
+const {login, invalidLogin,adminLogin} = require('../testing_utils/login.js');
 
 beforeAll(() => {
   const dbConfig = { useNewUrlParser: true, useUnifiedTopology: true };
-  mongoose.connect(process.env.TEST_DB, dbConfig, err => {
+  mongoose.connect(process.env.DB_URL, dbConfig, err => {
     if (err) {
       console.log(`Error: ${err.message}`);
     } else {
@@ -20,15 +21,40 @@ afterAll(() => {
 
 describe('Testing all the amdin vaccines data route', () => {
 
-  it('Returns status 200 when get all opening hour data', async done => {
+  it('Returns status 200 when get all services data', async done => {
+    const {token} = JSON.parse(await adminLogin());
     await request.get('/opening-hours/')
+      .set('token', token)
     expect(200); 
     done();
   });
 
 
+
+  it('Returns status 403 if not admin ', async done => {
+    const {token} = JSON.parse(await login());
+    await request.post('/opening-hours/')
+                .set('token', token)
+      expect(403);
+      done();
+  });
+
+
+
+  it('Returns status 403 if not admin ', async done => {
+    const {token} = JSON.parse(await login());
+    await request.post('/opening-hours/update')
+                .set('token', token)
+      expect(403);
+      done();
+  });
+
+
+
   it('Returns status 200 when the opennning hour data been updated successfully', async done => {
+    const {token} = JSON.parse(await login());
     await request.put('/opening-hours/update')
+                .set('token', token)
                 .send(
                   {
                     order: 'test-update-hour',
@@ -38,6 +64,24 @@ describe('Testing all the amdin vaccines data route', () => {
     expect(200); 
     done();
   });
+
+  it('Returns status 400 if admin and required fields missing', async done => {
+    const {token} = JSON.parse(await adminLogin());
+    await request.put('/opening-hours/update')
+                .set('token', token)
+      expect(400);
+      done();
+  });
+
+
+  it('Returns status 403 if not admin ', async done => {
+    const {token} = JSON.parse(await login());
+    await request.post('/opening-hours/show')
+                .set('token', token)
+      expect(403);
+      done();
+  });
+
 
   it('Returns status 200 when can show opening hour data', async done => {
     await request.get('/opening-hours/show')         
